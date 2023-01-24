62ef7ee7b6820f138ea08f84f7bff8aa55999ea5
my0866805011 <my0866805011@gmail.com>
2023-01-25 00:52:24 +0700

    ep-015

--- lib/screens/shop/add_info_shop.dart
+++ lib/screens/shop/add_info_shop.dart
@@ -1,3 +1,4 @@
+import 'package:appfood/utility/my_style.dart';
 import 'package:flutter/material.dart';
 
 class AddinfoShop extends StatefulWidget {
@@ -11,7 +12,96 @@ class _AddinfoShopState extends State<AddinfoShop> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Add Information Shop'),),
+      appBar: AppBar(
+        title: const Text('Add Information Shop'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            MyStyle().mySizedBox(),
+            nameForm(),
+            MyStyle().mySizedBox(),
+            addressForm(),
+            MyStyle().mySizedBox(),
+            phoneForm(),
+            MyStyle().mySizedBox(),
+            newImage(),
+          ],
+        ),
+      ),
     );
   }
+
+  Row newImage() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.add_a_photo,
+              size: 36.0,
+            )),
+        Container(
+          width: 250.0,
+          child: Image.asset('assets/images/shop.png'),
+        ),
+        IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.add_a_photo,
+              size: 36.0,
+            ))
+      ],
+    );
+  }
+
+  Widget nameForm() => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 250.0,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'ชื่อร้านค้า',
+                prefixIcon: Icon(Icons.account_box),
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+        ],
+      );
+
+  Widget addressForm() => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 250.0,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'ที่อยู่',
+                prefixIcon: Icon(Icons.home),
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+        ],
+      );
+
+  Widget phoneForm() => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 250.0,
+            child: TextField(
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                labelText: 'โทรติดต่อ',
+                prefixIcon: Icon(Icons.phone),
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+        ],
+      );
 }
